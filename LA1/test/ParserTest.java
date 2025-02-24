97acac3b3de0cb631065da5c56c4b776a5ab63c2
Colin Gale <colingale1201@gmail.com>
2025-02-23 21:28:12 -0700

    Made a welcome message, command list, and wrote a search song method for the view in Main.java. Small adjustments to ParserTest

--- LA1/test/ParserTest.java
+++ LA1/test/ParserTest.java
@@ -1,8 +1,10 @@
 package test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
+import model.Song;
 import model.Album;
 import model.Parser;
 
@@ -37,17 +39,17 @@ public class ParserTest {
 	
 	@Test
 	public void testMakeAlbum() {
-		Album myAlbum = Parser.makeAlbum("C:\\Users\\colin\\eclipse-workspace\\long-assignment-1\\LA1\\model\\19_Adele.txt");
+		Album myAlbum = Parser.makeAlbum("C:\\Users\\colin\\eclipse-workspace\\long-assignment-1\\LA1\\test\\albums\\19_Adele.txt");
 		assertEquals(myAlbum.getTitle(), "19");
 		assertEquals(myAlbum.getArtist(), "Adele");
 		assertEquals(myAlbum.getYear(), 2008);
-		String[] albumSongs = myAlbum.getSongs().toArray(new String[12]);
+		Song[] albumSongs = myAlbum.getSongs().toArray(new Song[12]);
 		String[] testSongs = {"Daydreamer", "Best for Last", "Chasing Pavements", "Cold Shoulder",
 		                         "Crazy for You", "Melt My Heart to Stone", "First Love", "Right as Rain",
 		                         "Make You Feel My Love", "My Same", "Tired", "Hometown Glory"};
 		
 		for (int i =0; i < albumSongs.length; i++) {
-			assertEquals(albumSongs[i], testSongs[i]);
+			assertEquals(albumSongs[i].getTitle(), testSongs[i]);
 		}
 		
 		
